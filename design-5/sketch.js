59ca1e32d13022c8664d8e93d2ad85157889073e
R1B0PR07008 <104583138+R1B0PR07008@users.noreply.github.com>
2023-12-04 19:39:04 -0800

    added new p5 project

---
+++ design-5/sketch.js
@@ -0,0 +1,22 @@
+let bg;
+
+let img1;
+let img2;
+let img3;
+
+function setup() {
+  createCanvas(1000, 600);
+
+  let img1;
+  bg = loadImage('./img/galaxy.jpeg')
+
+  img1 = loadImage('./img/pngimg.com - batman_PNG52.png');
+  img1 = loadImage('./img/spider-man-film-series-drawing-clip-art-3d-men-95e98fa659b8a52dc75ddca1e1c090c7.png');
+  img1 = loadImage('./img/Superhero-character-on-transparent-background-PNG.png');
+}
+
+function draw() {
+  background(bg)
+
+  
+}
