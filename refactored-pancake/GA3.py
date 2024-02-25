9b18b064323ca5a2354c55835f550373bf36771e
R1B0PR07008 <104583138+R1B0PR07008@users.noreply.github.com>
2024-02-25 14:22:51 -0600

    sure

--- refactored-pancake/GA3.py
+++ refactored-pancake/GA3.py
@@ -1,10 +1,13 @@
 from music21 import converter, instrument, note, chord
 from pathlib import Path
+from pitchControl import make_audios
 
 songs = []
-folder = Path('/home/r1b0ld1/Documents/GitHub/School-Public/refactored-pancake/data/sound')
+folder = Path('./data/sound')
 for file in folder.rglob('*.wav'):
     songs.append(file)
 
 for i,file in enumerate(songs):
     print(f'{i+1}: {file}')
+
+make_audios('./data/sound/clean-guitar-note_A_minor.wav')
